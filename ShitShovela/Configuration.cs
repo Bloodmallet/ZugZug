8035c39dc2bc5726d99c9884da601a80df586542
Peter Kruse <kruse.peter.1990@gmail.com>
2025-08-03 11:00:20 +0200

    add env var to run locally (into error)

---
+++ ShitShovela/Configuration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShitShovela
+{
+    internal class Configuration
+    {
+        internal readonly TimeSpan Duration;
+
+        private static TimeSpan GetDurationFromEnv() {
+            string duration_minutes = Environment.GetEnvironmentVariable( "DURATION_MINUTES" ) ?? "10";
+            try
+            {
+                Int32 parsed_duration_minutes = Int32.Parse( duration_minutes );
+                if (parsed_duration_minutes < 1)
+                {
+                    throw new ArgumentOutOfRangeException( "Environment variable DURATION_MINUTES must be a positive Int32." );
+                }
+                return new TimeSpan( hours: 0, minutes: parsed_duration_minutes, seconds: 0 );        
+            } catch (FormatException) {
+                Console.WriteLine( "Encountered an error when parsing DURATION_MINUTES from Environment. Value must be a positive Int32." );
+                throw;
+            }
+        }
+
+        internal Configuration()
+        {
+            Duration = GetDurationFromEnv();
+        }
+    }
+}
--- ShitShovela/Program.cs
+++ ShitShovela/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ShitShovela;
+
+internal class Program
+{
+    private async static Task<int> Main( string[] args )
+    {
+        Console.WriteLine( "Hello, World!" );
+
+        Configuration config = new Configuration();
+
+        return 0;
+    }
+}
