a60c0b4bd976e121be5b34d3e29f5646d3b36927
Peter Kruse <kruse.peter.1990@gmail.com>
2025-08-17 13:28:50 +0200

    add forgotten file

--- ShitShovela/Utils/EnvironmentAsTypeParser.cs
+++ ShitShovela/Utils/EnvironmentAsTypeParser.cs
@@ -1,87 +1,86 @@
 ﻿using System.Globalization;
 
-namespace ShitShovela.Utils
+namespace ShitShovela.Utils;
+
+/// <summary>
+/// Each provided method tries to convert the value of the given `key` to its announced type.
+/// In case the user input is not helping (null or empty string) or there is no environment variable with the given `key`, then null is returned.
+/// If the conversion to the wanted type fails, the method throws an error.
+/// </summary>
+internal static class EnvironmentAsTypeParser
 {
-    /// <summary>
-    /// Each provided method tries to convert the value of the given `key` to its announced type.
-    /// In case the user input is not helping (null or empty string) or there is no environment variable with the given `key`, then null is returned.
-    /// If the conversion to the wanted type fails, the method throws an error.
-    /// </summary>
-    internal static class EnvironmentAsTypeParser
+    internal static string? GetString( string key )
     {
-        internal static string? GetString( string key )
+        if ( string.IsNullOrWhiteSpace( key ) )
         {
-            if ( string.IsNullOrWhiteSpace( key ) )
-            {
-                return null;
-            }
-
-            Console.WriteLine( $"Reading environment variable '{key}'." );
-
-            string? value = Environment.GetEnvironmentVariable( key );
-
-            return string.IsNullOrWhiteSpace( value ) ? null : value;
+            return null;
         }
 
-        internal static float? GetFloat( string key )
+        Console.WriteLine( $"Reading environment variable '{key}'." );
+
+        string? value = Environment.GetEnvironmentVariable( key );
+
+        return string.IsNullOrWhiteSpace( value ) ? null : value;
+    }
+
+    internal static float? GetFloat( string key )
+    {
+        string? value = GetString( key );
+        if ( value == null )
         {
-            string? value = GetString( key );
-            if ( value == null )
-            {
-                return null;
-            }
-
-            if ( !float.TryParse( value, CultureInfo.InvariantCulture, out float parsedValue ) )
-            {
-                Console.WriteLine( $"Couldn't parse value of environment key '{key}' as float ." );
-                return null;
-            }
-
-            return parsedValue;
+            return null;
         }
 
-        internal static int? GetInt( string key )
+        if ( !float.TryParse( value, CultureInfo.InvariantCulture, out float parsedValue ) )
         {
-            string? value = GetString( key );
-            if ( value == null )
-            {
-                return null;
-            }
-
-            if ( !int.TryParse( value, CultureInfo.InvariantCulture, out int parsedValue ) )
-            {
-                Console.WriteLine( $"Couldn't parse value of environment key '{key}' as int ." );
-                return null;
-            }
-
-            return parsedValue;
+            Console.WriteLine( $"Couldn't parse value of environment key '{key}' as float ." );
+            return null;
         }
 
-        internal static TimeSpan? GetTimeSpan( string? hourKey, string? minuteKey, string? secondKey )
+        return parsedValue;
+    }
+
+    internal static int? GetInt( string key )
+    {
+        string? value = GetString( key );
+        if ( value == null )
         {
-            if ( string.IsNullOrWhiteSpace( hourKey ) && string.IsNullOrWhiteSpace( minuteKey ) && string.IsNullOrWhiteSpace( secondKey ) )
-            {
-                throw new ArgumentException( "At least one of the parameter of GetTimeSpan needs to contain a string." );
-            }
-
-            int hours = 0;
-            int minutes = 0;
-            int seconds = 0;
-
-            if ( !string.IsNullOrWhiteSpace( hourKey ) )
-            {
-                hours = GetInt( hourKey ) ?? hours;
-            }
-            if ( !string.IsNullOrWhiteSpace( minuteKey ) )
-            {
-                minutes = GetInt( minuteKey ) ?? minutes;
-            }
-            if ( !string.IsNullOrWhiteSpace( secondKey ) )
-            {
-                seconds = GetInt( secondKey ) ?? seconds;
-            }
-
-            return hours == 0 && minutes == 0 && seconds == 0 ? null : new TimeSpan( hours: hours, minutes: minutes, seconds: seconds );
+            return null;
         }
+
+        if ( !int.TryParse( value, CultureInfo.InvariantCulture, out int parsedValue ) )
+        {
+            Console.WriteLine( $"Couldn't parse value of environment key '{key}' as int ." );
+            return null;
+        }
+
+        return parsedValue;
+    }
+
+    internal static TimeSpan? GetTimeSpan( string? hourKey, string? minuteKey, string? secondKey )
+    {
+        if ( string.IsNullOrWhiteSpace( hourKey ) && string.IsNullOrWhiteSpace( minuteKey ) && string.IsNullOrWhiteSpace( secondKey ) )
+        {
+            throw new ArgumentException( "At least one of the parameter of GetTimeSpan needs to contain a string." );
+        }
+
+        int hours = 0;
+        int minutes = 0;
+        int seconds = 0;
+
+        if ( !string.IsNullOrWhiteSpace( hourKey ) )
+        {
+            hours = GetInt( hourKey ) ?? hours;
+        }
+        if ( !string.IsNullOrWhiteSpace( minuteKey ) )
+        {
+            minutes = GetInt( minuteKey ) ?? minutes;
+        }
+        if ( !string.IsNullOrWhiteSpace( secondKey ) )
+        {
+            seconds = GetInt( secondKey ) ?? seconds;
+        }
+
+        return hours == 0 && minutes == 0 && seconds == 0 ? null : new TimeSpan( hours: hours, minutes: minutes, seconds: seconds );
     }
 }
